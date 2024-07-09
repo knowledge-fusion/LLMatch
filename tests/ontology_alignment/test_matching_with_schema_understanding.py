59e0a9ac8a4ae7e4097d2c4eca3b252e11be33ca
knowledge-fusion <kbe83yehh6kpbwpkeblcvmzu4z76tb@bots.bitbucket.org>
2024-07-09 17:27:59 +0800

    no message

---
+++ tests/ontology_alignment/test_matching_with_schema_understanding.py
@@ -0,0 +1,13 @@
+def test_matching_with_schema_understanding():
+    run_specs = {
+        "source_db": "mimic_iii",
+        "target_db": "omop",
+        "matching_llm": "gpt-4o",
+        "rewrite_llm": "gpt-4o",
+        "strategy": "match_with_schema_understanding",
+        "template": "top2-no-na",
+    }
+
+    run_specs = {key: run_specs[key] for key in sorted(run_specs.keys())}
+    from llm_ontology_alignment.alignment_strategies.schema_understanding import run_matching_with_schema_understanding
+    run_matching_with_schema_understanding(run_specs)
