f4c6de9e197e657183f247ff8589ecf8aec8207e
knowledge-fusion <developer@knowledge-fusion.science>
2024-07-16 08:38:11 +0800

    no message

--- main.py
+++ main.py
@@ -37,8 +37,9 @@ def main():
     datasets = ["IMDB_Saki", "OMOP_Synthea", "OMOP_CMS", "OMOP_MIMIC"]
     models = ["gpt-4o", "gpt-3.5-turbo", "mistral-7b", "llama3-8b"]
     from llm_ontology_alignment.data_processors.rewrite_db_schema import rewrite_db_columns
-
-    rewrite_db_columns({"rewrite_llm": "gpt-4o"})
+    from llm_ontology_alignment.alignment_strategies.valentine_example import run_valentine
+    run_valentine()
+    # rewrite_db_columns({"rewrite_llm": "gpt-4o"})
     return
     # for item in list(
     #     SchemaEmbedding.objects(
